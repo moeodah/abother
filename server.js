2b62ec71c14bb61f752261179969b9dba214a65d
Mohammad <moeodah@gmail.com>
2022-03-24 14:20:33 -0400

    commit initial

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const serveStatic = require('serve-static')
 
 const app = express()
 
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
+app.use('/',serveStatic(path.join(__dirname,'/pages')))
 
 const port = process.env.PORT || 8080
 app.listen(port)
